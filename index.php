83005638fa6c9099dec17b9c67a26067bd853830
Laura Osorio <laurisosorio7@hotmail.com>
2020-10-22 18:42:27 -0500

    Iniciando repositorio

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda WEB1</title>
+</head>
+<body>
+   <H1>Subiendo Rpo</H1> 
+</body>
+</html>
