c05456690dcd42b5e08537edcc53c5d9de767796
Laura Osorio <laurisosorio7@hotmail.com>
2020-11-13 21:33:44 -0500

    Estilos

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
     <title>Tienda WEB1</title>
 </head>
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Tienda Los Ninjasos </a>
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <a class="navbar-brand  font-weight-bold" href="#">Tienda Los Ninjasos </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -20,16 +21,16 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" href="index.php">Agregar Producto </a>
+                <li class="nav-item ">
+                    <a class="nav-link active " href="index.php">Agregar Producto </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="listarProductos.php">Listar Productos</a>
+                    <a class="nav-link " href="listarProductos.php">Listar Productos</a>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <input class="form-control mr-sm-2 input" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
     </nav>
@@ -40,33 +41,33 @@
             <div class="form-row justify-content-center">
                 <div class="col-sm-6 col-md-3 mb-3">
                     <label>Nombre del Producto</label>
-                    <input type="text" class="form-control" placeholder="Chocolate" name="nombreProducto" required>
+                    <input type="text" class="form-control input" placeholder="Chocolate" name="nombreProducto" required>
                 </div>
                 <div class="col-sm-6 col-md-3 mb-3">
                     <label>Marca del producto</label>
-                    <input type="text" class="form-control" name="marcaProducto" placeholder="Corona" required>
+                    <input type="text" class="form-control input" name="marcaProducto" placeholder="Corona" required>
                 </div>
             </div>
             <div class="form-row justify-content-center">
                 <div class="col-sm-12 col-md-6 mb-3">
                     <label>Precio Producto</label>
-                    <input type="text" class="form-control" name="precio" required>
+                    <input type="text" class="form-control input" name="precio" required>
                 </div>
             </div>
             <div class="form-group justify-content-center">
                 <label>Descripción</label>
-                <textarea class="form-control col-ms-12 col-md-6 m-auto" name="descripcion" rows="3"
+                <textarea class="form-control col-ms-12 col-md-6 m-auto input" name="descripcion" rows="3"
                     required></textarea>
             </div>
 
             <div class="form-row justify-content-center">
                 <div class="col-sm-12 col-md-6 mb-3">
-                    <label>Foto Producto</label>
-                    <input type="text" class="form-control" name="foto">
+                    <label>Foto Producto (URL)</label>
+                    <input type="text" class="form-control input" name="foto">
                 </div>
             </div>
 
-            <button class="btn btn-primary" type="submit" name="registrar">Registrar</button>
+            <button class="btn btn-danger" type="submit" name="registrar">Registrar</button>
         </form>
     </div>
 
--- listarProductos.php
+++ listarProductos.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
     <title>Productos</title>
 </head>
 
@@ -24,7 +25,7 @@
     ?>
     <main>
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="#">Tienda Los Ninjasos </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,8 +42,8 @@
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <input class="form-control mr-sm-2 input" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
         </nav>
@@ -52,7 +53,7 @@
             <div class="row row-cols-1 row-cols-md-3">
                 <?php foreach($productos as $producto):?>
                 <div class="col mb-4">
-                    <div class="card">
+                    <div class="card input">
                         <img src="
                                 <?php echo($producto["imagenProducto"]) ?>
                             " alt=" <?php echo($producto["nombreProducto"]) ?>" />
@@ -88,37 +89,37 @@
                                         <span aria-hidden="true">&times;</span>
                                     </button>
                                 </div>
-                                <div class="modal-body">
+                                <div class="modal-body alerta">
                                     <form action="editarProducto.php?id=<?php echo($producto['idProducto'])?>"
                                         method="POST">
                                         <div class="form-row justify-content-center">
                                             <div class="col-sm-6 col-md-6 mb-3">
                                                 <label>Nombre del Producto</label>
-                                                <input type="text" class="form-control" name="nombreProducto"
+                                                <input type="text" class="form-control input" name="nombreProducto"
                                                     value="<?php echo($producto['nombreProducto'])?>">
                                             </div>
                                             <div class="col-sm-6 col-md-6 mb-3">
                                                 <label>Marca del producto</label>
-                                                <input type="text" class="form-control" name="marcaProducto"
+                                                <input type="text" class="form-control input" name="marcaProducto"
                                                     value="<?php echo($producto['marcaProducto'])?>">
                                             </div>
                                         </div>
                                         <div class="form-row justify-content-center">
                                             <div class="col-sm-12 col-md-12 mb-3">
                                                 <label>Precio Producto</label>
-                                                <input type="text" class="form-control" name="precio"
+                                                <input type="text" class="form-control input" name="precio"
                                                     value="<?php echo($producto['precioProducto'])?>">
                                             </div>
                                         </div>
                                         <div class="form-group justify-content-center">
                                             <label>Descripción</label>
-                                            <textarea class="form-control col-md-12 col-md-6 m-auto" name="descripcion"
+                                            <textarea class="form-control col-md-12 col-md-6 m-auto input" name="descripcion"
                                                 rows="3"><?php echo($producto['descripcionProducto'])?></textarea>
                                         </div>
                                         <div class="form-row justify-content-center">
                                             <div class="col-sm-12 col-md-12 mb-3">
-                                                <label>Foto Producto</label>
-                                                <input type="text" class="form-control" name="foto"
+                                                <label>Foto Producto (URL)</label>
+                                                <input type="text" class="form-control input" name="foto"
                                                     value="<?php echo($producto['imagenProducto'])?>">
                                             </div>
                                         </div>
